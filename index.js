7ad086204b824b57c3b675f2a934c5d6fadd2c68
Ubuntu <ubuntu@ip-172-31-16-108.us-east-2.compute.internal>
2020-04-19 03:07:09 +0000

    return random url from api

--- index.js
+++ index.js
@@ -3,11 +3,30 @@ addEventListener("fetch", (event) => {
 });
 
 /**
- * Respond with hello worker text
+ * Respond to the request
  * @param {Request} request
  */
 async function handleRequest(request) {
-  return new Response("Hello worker!", {
-    headers: { "content-type": "text/plain" },
-  });
+  const apiUrls = await requestUrls();
+  const resUrl = apiUrls[getRandomIndex(apiUrls.length)];
+  console.log(resUrl);
+  return new Response(resUrl, { status: 200 });
+}
+
+/*
+ * Get two URLs from API
+ */
+async function requestUrls() {
+  const apiUrl = "https://cfw-takehome.developers.workers.dev/api/variants";
+  const res = await fetch(apiUrl);
+  const json = await res.json();
+  return [...json.variants];
+}
+
+/*
+ * Return random integer from 0 to max
+ * @param {Number} max integer not inclusive
+ */
+function getRandomIndex(maxInt) {
+  return Math.floor(Math.random() * Math.floor(maxInt));
 }
